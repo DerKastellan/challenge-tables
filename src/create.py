9c8f0dfdf4d3c243b459cfd9c221e3f921eaa7c3
Oliver Korpilla <oliver.korpilla@nokia.com>
2016-05-08 16:30:05 +0200

    interim commit

---
+++ src/create.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from parser import parseTables
+from tables import ChallengeTable
+
+
+if __name__ == "__main__":
+    uri = "http://dnd.wizards.com/products/tabletop/dm-basic-rules"
+
+    print("... parsing table content from URI", uri)
+    thresholds, multipliers = parseTables(uri)
+    print("... parsing table content from URI done")
+
+    print("... computing challenge table")
+    table = ChallengeTable(thresholds, multipliers)
+    table.compute([1, 1, 1, 1])
+    table.compute([5, 5, 5, 5, 5])
